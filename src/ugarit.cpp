01874a686940cf6fc510b91f775d331cef0f2384
Christian Kotz <christian.kotz@itk-engineering.com>
2024-06-21 15:27:09 +0200

    start porting to C++ modules

--- src/ugarit.cpp
+++ src/ugarit.cpp
@@ -1,1 +1,25 @@
-#include <ugarit/ugarit.hpp>
+module;
+#include <concepts>
+#include <cstdint>
+#include <type_traits>
+
+export module ugarit;
+
+export{
+    namespace ugarit {
+    using bits = std::uintmax_t;
+
+    template <typename T>
+    concept Arithmetic = std::is_arithmetic_v<T>;
+
+    template <typename T>
+    concept bladesig = requires(T t) {
+        { T::sig } -> std::same_as<bits>;
+    };
+
+    template <typename T> struct bb;
+
+    template <Arithmetic T> struct bb<T> : std::integral_constant<bits, bits{}>{};
+
+    }
+}
