bcaade7d923d1d45ed54bc7d9663c929f625fe9e
Christian Kotz <christian.kotz@itk-engineering.com>
2024-07-03 09:30:23 +0200

    add first draft of metaprogramming with KVASIR style HOMF

---
+++ include/ugarit/meta.hpp
@@ -0,0 +1,68 @@
+#pragma once
+#include <concepts>
+#include <cstdint>
+
+namespace ugarit {
+
+    // concepts
+
+    template <typename T>
+    concept MetaFunction = requires {
+        typename T::type;
+    };
+
+    template <typename T, typename... TS>
+    concept HigherOrderMetaFunction = requires {
+        typename T::template f<TS...>;
+    };
+
+    template <typename T> 
+    concept Continuation = HigherOrderMetaFunction<T>;
+
+    // algorithms
+
+    // branch among a number of alternatives
+    template <unsigned, Continuation C = listify> struct select;
+    template <unsigned n, Continuation C = listify> struct select{
+         template <typename T, typename... TS> using f = select<n-1>::template f<TS...>; };
+    template <Continuation C> struct select<0U,C>{ template <typename T, typename...> using f = C::template f<T>; };
+    
+    // branch
+    template <bool c, typename Yes, typename No> using cond = select<unsigned(c)>::template f<Yes, No>;
+
+    // constant
+    template <typename T, Continuation C = listify> 
+    struct lift {
+        template <typename...> using f = C:: template f<T>; 
+    };
+
+    // constant
+    template <unsigned... ns> struct nlist;
+
+    struct listify {
+        template <typename... TS> using f = list<TS...>;      
+    };
+
+    template <Continuation C = listify>
+    struct call {
+        template <typename... TS> using f = C::template f<TS...>;      
+    };
+
+    template <Continuation C = listify>
+    struct call {
+        template <typename... TS> using f = C::template f<TS...>;      
+    };    
+
+    template <HigherOrderMetaFunction TF, Continuation C = listify>
+    struct transform {
+        template <typename... TS> using f = C::template f<TF::template f<TS>...>;      
+    };
+
+    template <HigherOrderMetaFunction TF, Continuation C = listify>
+    struct find {
+        template <typename... TS> using f = C::template f<TF::template f<TS>...>;      
+    };
+
+
+
+}
